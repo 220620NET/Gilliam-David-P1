0e228aa7be78dceddbd62c93d7393acba54349cf
David <dave22011@yahoo.com>
2022-06-23 21:16:10 -0400

    Added User Model Class

--- Project-1/Program.cs
+++ Project-1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+User newUser = new User("Sammy", 25, "house566", "Employee");
+//newUser.UserName = "Billy";
+
+
+Console.WriteLine("Username: " + newUser.UserName);
+Console.WriteLine("User ID: " + newUser.UserID);
+Console.WriteLine("Password: " + newUser.Password);
+Console.WriteLine("Role: " + newUser.Role);
